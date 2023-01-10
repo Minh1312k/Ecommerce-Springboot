a25fd85b6d110b829f7848b5e4ed641ac2db2054
84963 <phamhuutuanminh1312000@gmail.com>
2023-01-10 16:42:22 +0700

    Language levels error

---
+++ Library/src/main/java/com/ecommerce/library/dto/AdminDto.java
@@ -0,0 +1,26 @@
+package com.ecommerce.library.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminDto {
+    @Size(min = 3, max = 10, message = "Invalid first name! (3-10 characters)")
+    private String firstName;
+
+    @Size(min = 3, max = 10, message = "Invalid last name! (3-10 characters)")
+    private String lastName;
+
+    private String userName;
+
+    @Size(min = 3, max = 10, message = "Invalid password! (3-10 characters)")
+    private String password;
+
+    private String repeatPassword;
+}
